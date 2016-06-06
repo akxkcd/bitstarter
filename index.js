787e423fc3b7b9ec45d6fdf9b3409835d24576a3
akxkcd <anurag.iitkgp@gmail.com>
2016-06-06 06:11:18 +0000

    updated path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
-var fs = require('fs');
-var pg = require('pg');
+var fs = require('fs')
+var pg = require('pg')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
