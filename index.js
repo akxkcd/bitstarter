e7caaa387cdbed4453cc219b758a29938c78eb26
akxkcd <anurag.iitkgp@gmail.com>
2016-06-07 04:21:20 +0000

    updated

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
+var fs = require('fs')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-//  var buffer = new Buffer(fs.readFileSync('index.html'));
-//  response.send(buffer.toString('utf-8'));
+//  response.send('Hello World 2!')
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+  response.send(buffer.toString('utf-8'));
 })
 
 app.listen(app.get('port'), function() {
